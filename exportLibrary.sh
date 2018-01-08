59a2dbe7678c8d8e947cf2d14e4a393565667db3
glecun <gregoire.lecun@vidal.fr>
2018-01-08 12:20:08 +0100

    Changed "like" playlist to XXXWWWYYY Modified some printed texts Added tests for songs not unique Modified when test "music manually added" execute

--- exportLibrary.sh
+++ exportLibrary.sh
@@ -2,18 +2,17 @@
 nbLignesBibliotheque=$( grep "Library" playlists_export.csv | awk -F "\"*;;\"*" '{print $6}' | grep ^T | wc -l );
 nbLignesJaime=$( grep "XXXYYYWWW" playlists_export.csv | awk -F "\"*;;\"*" '{print $6}' | grep ^T | wc -l );
 printf "Nombre de titres dans Bibliotheque: $nbLignesBibliotheque \n";
-printf "Nombre de titres dans J'aime : $nbLignesJaime \n";
+printf "Nombre de titres dans XXXYYYWWW* : $nbLignesJaime \n";
 
 #Number of difference
-let nbDiff=$nbLignesBibliotheque-$nbLignesJaime;
-printf "\nDifferenced: $nbDiff \n\n";
+nbDiff=$(($nbLignesBibliotheque-$nbLignesJaime));
+printf "\nDifference: $nbDiff \n\n";
 
-#Number of music manually added
-nbLignesManuallyAdded=$( cat playlists_export.csv | awk -F "\"*;;\"*" '{print $6}' | grep ^[^T]  | wc -l );
-printf "Nombre de titres ajoute manuellement: $nbLignesManuallyAdded \n\n";
-
-printf "\n\n############# Voici les titres ajoute manuellement : \n"
-cat playlists_export.csv | awk -F "\"*;;\"*" '{print $6}' | grep ^[^T]  | grep -f - playlists_export.csv
+#Songs not unique
+printf "\n\n############# Voici les titres en double dans la bibliotheque: \n"
+grep "Library" playlists_export.csv | awk -F "\"*;;\"*" '{print $6}' | grep ^T | sort | uniq -d	| grep -f - playlists_export.csv | grep "Library"
+printf "\n\n############# Voici les titres en double dans les playlists XXXYYYWWW*: \n"
+grep "XXXYYYWWW" playlists_export.csv | awk -F "\"*;;\"*" '{print $6}' | grep ^T | sort | uniq -d | grep -f - playlists_export.csv | grep "XXXYYYWWW"
 
 if [ "$nbDiff" -ne 0 ]; then
 	#If there is some differences
@@ -22,19 +21,26 @@ if [ "$nbDiff" -ne 0 ]; then
 
 	printf "\n\n############# Voici les titres manquants dans la bibliotheque: \n"
 	grep -iFxv -f librarytmp.txt thumbsuptmp.txt | grep -f - playlists_export.csv
-	printf "\n\n############# Voici les titres manquants dans la playlist J'aime: \n"
+	printf "\n\n############# Voici les titres manquants dans les playlists XXXYYYWWW*: \n"
 	grep -iFxv -f thumbsuptmp.txt librarytmp.txt | grep -f - playlists_export.csv
-	
+
 	rm librarytmp.txt
 	rm thumbsuptmp.txt
 
 else
 	#If there isn't any differences
-	printf "\nAucuned differences\n\n";
+
+	#Number of music manually added
+	nbLignesManuallyAdded=$( cat playlists_export.csv | awk -F "\"*;;\"*" '{print $6}' | grep ^[^T] | grep -f - playlists_export.csv | grep "Library" | wc -l );
+	printf "\n\nNombre de titres ajoute manuellement: $nbLignesManuallyAdded";
+
+	printf "\n\n############# Voici les titres ajoute manuellement : \n"
+	cat playlists_export.csv | awk -F "\"*;;\"*" '{print $6}' | grep ^[^T] | grep -f - playlists_export.csv | grep "Library"
+
+	printf "\nAucune difference\n\n";
 	nbLignes=$( grep "Library" playlists_export.csv | wc -l );
 	printf "Exportation de $nbLignes titres: " ;
 	grep "Library" playlists_export.csv > library.csv ;
 	printf "Done ! \n";
 
 fi
-
